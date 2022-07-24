2e3ab23fbe801be57936c1b816a81cad717dc50a
SAIF-ANWAR <saifanwar5123@gmail.com>
2022-07-25 00:55:41 +0600

    animation

--- src/pages/Home/Landing.js
+++ src/pages/Home/Landing.js
@@ -1,19 +1,35 @@
-import React from 'react';
-import img from '../../assets/image/bucketgirl.png'
-
+import React, { useEffect } from 'react';
+import img from '../../assets/image/bucketgirl.png';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 const Landing = () => {
+    useEffect(() => {
+        AOS.init();
+    }, [])
     return (
         <>
             <div class="hero h-screen lg:h-[62vh] bg-accent mt-16">
                 <div class="hero-content flex-col lg:flex-row">
                     <div>
-                        <p>Best Quality</p>
-                        <h1 class="text-5xl font-bold">Professional Cleaning Service</h1>
-                        <p class="py-6 max-w-xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button class="btn btn-primary">Get Started</button>
+                        <p data-aos="fade-right"
+                            data-aos-duration="1000"
+                            data-aos-delay="200"
+                        >Best Quality</p>
+                        <h1 data-aos="fade-right"
+                            data-aos-duration="1000"
+                            data-aos-delay="400"
+                            class="text-5xl font-bold">Professional Cleaning Service</h1>
+                        <p data-aos="fade-right"
+                            data-aos-duration="1000"
+                            data-aos-delay="600"
+                            class="py-6 max-w-xl">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button data-aos="zoom-in"
+                            data-aos-duration="1000"
+                            data-aos-delay="800"
+                            class="btn btn-primary">Get Started</button>
                     </div>
                     <div className='h-[60vh] shrink-0'>
-                        <img src={img} class="h-full" alt='' />
+                        <img src={img} data-aos="fade-left" data-aos-duration="1000" class="h-full" alt='' />
                     </div>
                 </div>
             </div>
@@ -29,6 +45,7 @@ const Landing = () => {
                     <input type="text" placeholder="Type here" class="input input-bordered w-full" />
                     <input type="text" placeholder="Type here" class="input input-bordered w-full" />
                 </div>
+                <button className='btn btn-primary mt-5'>Get a Quote</button>
             </div>
         </>
 
